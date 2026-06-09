b78df66b99d61ac0220e4ec9b7f9c2af791fd2e4
Arya raditya pasha <email.aryaradityapasha@gmail.com>
2026-06-09 21:50:28 +0700

    nambah migrasi create order

---
+++ database/migrations/2026_06_09_144723_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('idproduct')->references('id')->on('products');
+            $table->foreignId('iduser')->references('id')->on('users')->onDelete('cascade');
+            $table->string('metode_pembayaran');
+            $table->integer('total_pembayaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
