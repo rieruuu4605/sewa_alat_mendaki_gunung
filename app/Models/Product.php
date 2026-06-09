31aed77eeb3470b613ac5905ce65cc5e6b89aae1
Yosep Sudirman <yosepsudirman07@example.com>
2026-06-09 23:13:28 +0700

    ubah isi model produk

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,20 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
-    protected $fillable =[
+    protected $fillable = [
         'namaproduct',
         'gambar',
         'deskripsi',
         'harga',
-        'stok'
+        'stok',
+        'kategori' // tambah kolom kategori
     ];
 
     public function cart()
     {
-        return $this->hasOne(Cart::class,'idproduct','id');
+        return $this->hasOne(Cart::class, 'idproduct', 'id');
     }
+
     public function order()
     {
-        return $this->hasOne(Order::class,'idproduct','id');
+        return $this->hasOne(Order::class, 'idproduct', 'id');
     }
 }
