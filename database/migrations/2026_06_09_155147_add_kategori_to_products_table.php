77608fb0e21dc96bf283de2b9d11460e14d08fb8
Yosep Sudirman <yosepsudirman07@example.com>
2026-06-09 22:52:40 +0700

    ubah isi modifu user

--- database/migrations/2026_06_09_155147_add_kategori_to_products_table.php
+++ database/migrations/2026_06_09_155147_add_kategori_to_products_table.php
@@ -9,18 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->string('phonenumber')->nullable()->change();
-    });
-}
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
     }
 };
