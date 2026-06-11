92737ee6838c89264adffe874ca9c18c3d2186ac
Arya raditya pasha <email.aryaradityapasha@gmail.com>
2026-06-11 10:46:16 +0700

    nambah please-login

---
+++ resources/views/please-login.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div style="text-align: center; margin-top: 50px;">
+        <h1>Ups!</h1>
+        <p>Kamu harus login terlebih dahulu untuk melakukan transaksi.</p>
+        <a href="/login" class="btn btn-primary">Klik di sini untuk Login</a>
+    </div>
+</body>
+</html>
