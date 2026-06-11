36ba405a28a082d8ac32a76525564e86781ed248
Yosep Sudirman <yosepsudirman07@gmail.com>
2026-06-11 23:31:02 +0700

    fix kuning

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
 
     public function logout(Request $request)
     {
-        auth()->logout();
+        Auth::logout();
         
         $request->session()->invalidate();
         $request->session()->regenerateToken();
